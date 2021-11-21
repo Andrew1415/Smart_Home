60a161a3c7b97f17fcae5fe80ae7f213551e35a3
Andy <andrius1415@gmail.com>
2021-11-21 19:32:31 +0200

    .

--- alerts.js
+++ alerts.js
@@ -12,3 +12,39 @@ if (alertTrigger) {
     alert('Nice, you triggered this alert message!', 'success')
   })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var toastTrigger_front_door_lock = document.getElementById('front_lock')
+var toastTrigger_front_door_unlock = document.getElementById('front_unlock')
+var toastLive_front_door_lock = document.getElementById('liveToast1')
+var toastLive_front_door_unlock = document.getElementById('liveToast2')
+if (toastTrigger_front_door_lock) {
+  toastTrigger_front_door_lock.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLive_front_door_lock)
+
+    toast.show()
+  })
+}
+if (toastTrigger_front_door_unlock) {
+  toastTrigger_front_door_unlock.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLive_front_door_unlock)
+
+    toast.show()
+  })
+}
+*/
