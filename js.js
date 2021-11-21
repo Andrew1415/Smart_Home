e97dd8e5272af3c69a896ed187f0412fe21c59bf
Andy <andrius1415@gmail.com>
2021-11-21 21:32:40 +0200

    .

--- js.js
+++ js.js
@@ -10,10 +10,26 @@ function showMenu(menuId)  {
     document.getElementById('menu8').style.display="none";
     document.getElementById('menu9').style.display="none";
     document.getElementById('menu10').style.display="none";
+    document.getElementById('menu11').style.display="none";  
+  
     //display the menu for this area of the image
     document.getElementById(menuId).style.display="block";
     }
-function hideMenu(menuId){
+
+function florePlan(menuId){
+
+    document.getElementById('menu11').style.display="none";
+    document.getElementById('menu12').style.display="none";
+    document.getElementById('menu13').style.display="none";
+    document.getElementById('menu14').style.display="none";
+    document.getElementById('menu15').style.display="none";
+
+    document.getElementById(menuId).style.display="block";
+}
+/*
+function hideMenu(menuId){  
+    
+
     document.getElementById('menu1').style.display="none";
     document.getElementById('menu2').style.display="none";
     document.getElementById('menu3').style.display="none";
@@ -23,6 +39,15 @@ function hideMenu(menuId){
     document.getElementById('menu7').style.display="none";
     document.getElementById('menu8').style.display="none";
     document.getElementById('menu9').style.display="none";
-    document.getElementById('menu10').style.display="block";
+    document.getElementById('menu10').style.display="none";
 }
+/*
 
+
+   document.getElementById('MasterBedroom').style.display="none";
+    document.getElementById('HomeOffice').style.display="none";
+    document.getElementById('Kitchen').style.display="none";
+    document.getElementById('BathroomWC').style.display="none";
+    document.getElementById('Corridor').style.display="none";
+    
+    */
