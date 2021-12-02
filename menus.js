5aa4dd4bcf01acacdae80a1bb479ef22bafa7d10
Andy <andrius1415@gmail.com>
2021-12-02 16:25:08 +0200

    clean up

---
+++ menus.js
@@ -0,0 +1,27 @@
+function showMenu(menuId)  {
+    //hide all the menues
+    document.getElementById('menu1').style.display="none";
+    document.getElementById('menu2').style.display="none";
+    document.getElementById('menu3').style.display="none";
+    document.getElementById('menu4').style.display="none";
+    document.getElementById('menu5').style.display="none";
+    document.getElementById('menu6').style.display="none";
+    document.getElementById('menu7').style.display="none";
+    document.getElementById('menu8').style.display="none";
+    document.getElementById('menu9').style.display="none";
+    document.getElementById('menu10').style.display="none";
+  
+    //display the menu for this area of the image
+    document.getElementById(menuId).style.display="block";
+    }
+
+function florePlan(menuId){
+
+    document.getElementById('menu11').style.display="none";
+    document.getElementById('menu12').style.display="none";
+    document.getElementById('menu13').style.display="none";
+    document.getElementById('menu14').style.display="none";
+    document.getElementById('menu15').style.display="none";
+
+    document.getElementById(menuId).style.display="block";
+}
